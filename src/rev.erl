3b0616cffe2d7e498300e278b9567d3d6c528cde
Santiago Verdu <santiago@cl.ly>
2022-10-21 23:26:12 -0600

    um

--- src/rev.erl
+++ src/rev.erl
@@ -5,6 +5,9 @@
 -record(state, {
                }).
 
+init(Req0=#{method = <<"GET">>}, _Opts) ->
+  Req = cowboy_req:reply(200, #{}, <<"get">>, Req0),
+  {ok, Req, #state{}}
 init(Req0, _Opts) ->
     Req = cowboy_req:reply(200,
                            #{<<"content-type">> => <<"application/json">>},
