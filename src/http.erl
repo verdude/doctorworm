884e540f02e77bf7d49676b77ccf4ea16e8c48cc
Santiago Verdu <santi@getlovage.com>
2022-10-25 11:17:19 -0600

    reply

--- src/http.erl
+++ src/http.erl
@@ -9,10 +9,11 @@ init(Req0=#{method := <<"GET">>}, _Opts) ->
   Req = proc:read(Req0),
   {ok, Req, #{}};
 init(Req0=#{method := <<"OPTIONS">>}, _Opts) ->
-  Req = cowboy_req:set_resp_headers(#{
+  Req1 = cowboy_req:set_resp_headers(#{
                                           <<"access-control-allow-origin">> => <<"*">>,
                                           <<"access-control-allow-methods">> => <<"GET, PUT">>
                                          }, Req0),
+  Req = cowboy_req:reply(200, Req1),
   {ok, Req, #{}};
 init(Req0, _Opts) ->
   Req = cowboy_req:reply(200,
